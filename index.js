55a77ea48aa3b4d8b59bb9dc951c362942e7b146
xauvk <xauvkubalexanderyang@live.com>
2022-07-07 09:06:05 -0400

    commit tree ddf894973954525f5bb6102bccdcdb1d7b121783

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Your code goes here
+const textElement = document.querySelector('#text')
+console.log(textElement)
+textElement.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
